4bba58de2effe42e01666508a785192bfd3c0d1c
Marryanne Shintaku <mkinjo412@gmail.com>
2024-11-25 08:57:04 -0800

    final push

--- MS_Work/api_keys.py
+++ MS_Work/api_keys.py
@@ -1,5 +1,8 @@
 # OpenWeatherMap API Key
-weather_api_key = "6d98087520cd90538ffc2f921c78a39a"
+mapmyindia_key = "5c26d65492dff918d006eeb5f28cdfe7"
 
 # Geoapify API Key
 geoapify_key = "b08b5a52661742f0b8a75c4d6e42fd0a"
+
+# OpenWeatherMap API Key
+weather_api_key = "6d98087520cd90538ffc2f921c78a39a"
